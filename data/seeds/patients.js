d5187b3b2a430c4475064c552336992a7f8f483d
MisterC-Rogers <c.a.r0g3r5@gmail.com>
2020-02-09 01:52:21 -0800

    seed table fix

--- data/seeds/patients.js
+++ data/seeds/patients.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('patients').truncate()
+  return knex('patients').del()
     .then(function () {
       // Inserts seed entries
       return knex('patients').insert([
